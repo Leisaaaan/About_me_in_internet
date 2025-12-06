aa39f089ed937152f9ecce2994495fa81e8c3e1e
Leisaaan <zakirova_developer@mail.ru>
2025-12-06 14:22:18 +0300

    Создан еще один файл: service. В service содержится логика на чистом Python (пока только функция выдачи всей имеющейся базы данных), в этом же файле  пока мини-база данных для проверки работоспособности кода. Файл main, созданные ранее содержит Flask и является точкой входа в интернет, позволяет вывести на главную страницу приветствие, а также список всех полезных материалов

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from service import service
 
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET"])
 def hello():
     return "Hello"
+
+@app.route("/useful_information", methods = ["GET"])
+def get_all_information():
+    info = service.get_all_information()
+    return info
 
 if __name__ == "__main__":
     app.run(debug = True, port = 5000)
