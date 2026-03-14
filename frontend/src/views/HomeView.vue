8315dc9d2f7f319379da7dadde7a6051c51e4529
Leisaaaaan <zakirova_developer@mail.ru>
2026-03-14 09:56:44 +0300

    Clean up project from example of Vue. Now it is ready for my code

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>Это моя главная страница</h1>
   </main>
 </template>
